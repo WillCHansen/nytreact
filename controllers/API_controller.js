62b6f73cca26e5be78579d95d5007630aa5591af
Will Hansen <william.hansen@millenniumhealth.com>
2017-06-30 11:55:48 -0700

    working on API-controller

--- controllers/API_controller.js
+++ controllers/API_controller.js
@@ -3,19 +3,39 @@ var Article = require("../models/Article.js");
 
 module.exports = function(app){
 	app.get("/api/saved", function(req,res){
+		console.log("routing to api/saved")
 		/* FIND ALL QUERY find all 'saved articles' */
+		Article.find({}).exec(function(err,doc){
+			if(err){
+				console.log(err)
+			} else {
+				console.log(doc);
+				res.send(doc)
+			}
+		})
 	});
 
 	app.post("/api/saved", function(req,res){
+		console.log(" /api/saved post route called");
 		/* ADD an article to the database */
+		var article = new Article (req.body);
+		article.save(function(err,results){
+			if(err){
+				throw err;
+			} else {
+				console.log("article added");
+				console.log(results);
+				res.send("article added");
+			}
+		})
+
 	});
 
-	app.delete("/api/saved", function(req,res){
+	app.post("/api/saved/delete", function(req,res){
 		/* REMOVE an article from the database */
-	})
+		Article.findById(req.body.id)
+		.remove().exec(res.send("article removed"));
 
-	app.get("/", function(req,res){
-		/*Servers home page*/
-		res.send(/*index.html*/);
-	})
+		console.log("the "+req.body.id+" document has been DELETED");
+	});
 }
